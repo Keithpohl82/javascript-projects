052ed8e032c5c006e84b5ae8226632bb627d9ccd
keithpohl82 <keith.pohl82@gmail.com>
2024-06-24 08:20:33 -0500

    forloop exercise

--- loops/exercises/for-Loop-Exercises.js
+++ loops/exercises/for-Loop-Exercises.js
@@ -1,20 +1,46 @@
 /*Exercise #1: Construct for loops that accomplish the following tasks:
     a. Print the numbers 0 - 20, one number per line.
+    
     b. Print only the ODD values from 3 - 29, one number per line.
     c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
     d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers). */
+    for (let i = 0; i < 21; i++) {
+      console.log(i);
+     }
 
+    for (i = 3; i <= 29; i++){
+      if (i % 2 !== 0) {
+        console.log(i);
+      }
+    }
 
+    for ( i = 12; i >= -14; i--) {
+      if (i % 2 === 0) {
+          console.log(i); 
+      }
+  }
 
+    for (i = 50; i >= 20; i--) {
+      if (i % 3 === 0) {
+        console.log(i);
+      }
+    }
 
 /*Exercise #2: 
-Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].
+Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].*/
+let str = "LaunchCode";
+let arr = [1, 5, "LC101", "blue", 42];
 
-
-Construct ``for`` loops to accomplish the following tasks:
+/*Construct ``for`` loops to accomplish the following tasks:
   a. Print each element of the array to a new line.
   b. Print each character of the string - in reverse order - to a new line. */ 
+    for (i = 0; i <= arr.length -1; i++) {
+      console.log(arr[i]);
+    }
 
+    for (i = 0; i < str.length; i++) {
+      console.log(str[i]);
+    }
 
 
 
@@ -22,3 +48,16 @@ Construct ``for`` loops to accomplish the following tasks:
 /*Exercise #3:Construct a for loop that sorts the array [2, 3, 13, 18, -5, 38, -10, 11, 0, 104] into two new arrays:
   a. One array contains the even numbers, and the other holds the odds.
   b. Print the arrays to confirm the results. */
+
+  let startingArray = [2, 3, 13, 18, -5, 38, -10, 11, 0, 104];
+  let evenArry = [];
+  let oddArry = [];
+  for (i = 0; i < startingArray.length; i++) {
+    if (startingArray[i] % 2 === 0) {
+      evenArry.push(startingArray[i]);
+    } else {
+      oddArry.push(startingArray[i]);
+    }
+  }
+  console.log(evenArry);
+  console.log(oddArry);
