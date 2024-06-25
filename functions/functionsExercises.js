c76e62ae6edf2d735e145e931223cf814ca48eba
keithpohl82 <keith.pohl82@gmail.com>
2024-06-24 22:48:43 -0500

    Setup all challenges for functions exercises

--- functions/functionsExercises.js
+++ functions/functionsExercises.js
@@ -1,3 +1,4 @@
+//////////makeline function////////////
 function makeLine(size){
     let line = "";
     for(i = 0; i < size; i++){
@@ -5,5 +6,42 @@ function makeLine(size){
     }
     return line;
 }
+console.log(makeLine(28));
 
-makeLine(8);
+
+/////////makeSquare function////////
+function makeSquare(size){
+
+}
+console.log(makeSquare(4));
+
+
+/////////makeRectangle function////////
+function makeRectangle(width, height){
+
+}
+console.log(makeRectangle(2,7));
+
+
+/////////makeDownwardStairs function////////
+function makeDownwardStairs(height){
+
+}
+
+
+/////////makeSpaceLine function////////
+function makeSpaceLine(numSpaces, numChars){
+
+}
+
+
+/////////makeIsoscelesTriangle function////////
+function makeIsoscelesTriangle(height){
+
+}
+
+
+/////////makeDiamond function////////
+function makeDiamond(height){
+
+}
