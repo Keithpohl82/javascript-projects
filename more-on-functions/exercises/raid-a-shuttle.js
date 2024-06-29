1e15d7db5d2f13f73179f26a1d069c5b2c32cf47
keithpohl82 <keith.pohl82@gmail.com>
2024-06-29 12:06:34 -0500

    raid started

--- more-on-functions/exercises/raid-a-shuttle.js
+++ more-on-functions/exercises/raid-a-shuttle.js
@@ -28,7 +28,17 @@ console.log("Hold status: " + holdStatus(cargoHold));
  */
  
 //a). Define an anonymous function and set it equal to a variable with a normal, non-suspicious name. The function takes one parameter. This will be the fuel level on the shuttle.
-
+let recyclePipe = function (param){
+  if (checkFuel(param === 'green')) {
+    return param - 100001;
+  } else if (checkFuel(param === 'yellow')) {
+    return param - 5001;
+  } else {
+    return param;
+  }
+ 
+}
+console.log(recyclePipe(fuelLevel));
 //b). You must siphon off fuel without alerting the TAs. Inside your function, you want to reduce the fuel level as much as possible WITHOUT changing the color returned by the checkFuel function.
 
 //c). Once you figure out how much fuel to pump out, return that value.
