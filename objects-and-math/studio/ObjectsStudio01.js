c9f47d3d726e3e0820d7e05a7af12dac2fafd7ad
keithpohl82 <keith.pohl82@gmail.com>
2024-07-08 23:06:33 -0500

    Struggeling with this studio

--- objects-and-math/studio/ObjectsStudio01.js
+++ objects-and-math/studio/ObjectsStudio01.js
@@ -1,10 +1,39 @@
 // Code your selectRandomEntry function here:
+let idNumbers = [291, 414, 503, 599, 796, 890];
+
+
+
+function selectRandomEntry(arrayInput){
+  let randomID = Math.floor(Math.random() * arrayInput.length);
+  return idNumbers[randomID];
+}
+
+
+let selectedCrew = [];
+  
+while (selectedCrew.length < 3){
+  let pickedMember = selectRandomEntry(idNumbers);
+    if (!selectedCrew.includes(pickedMember)) {
+      selectedCrew.push(pickedMember);
+    }  
+  }
+
+  console.log(selectedCrew);
 
 
 // Code your buildCrewArray function here:
+function buildCrewArray(randomIdArray, candidateArray){
+  let crewNames = [];
+ 
+  for(i = 0; i < randomIdArray.length; i++){
+    //console.log(randomIdArray[i]);
+    if(candidateArray.includes(randomIdArray[i])){
+      console.log(i); 
+    }
+  }
+  
+}
 
-
-let idNumbers = [291, 414, 503, 599, 796, 890];
 
 // Here are the candidates and the 'animals' array:
 let candidateA = {
@@ -53,3 +82,7 @@ let candidateF = {
 let animals = [candidateA,candidateB,candidateC,candidateD,candidateE,candidateF];
 
 // Code your template literal and console.log statements:
+buildCrewArray(selectedCrew, animals);
+//console.log(`${buildCrewArray(selectedCrew[0].name)}, ${buildCrewArray(selectedCrew[1].name)}, and ${buildCrewArray(selectedCrew[2].name)} are going to space!`)
+
+//console.log(`${buildCrewArray(selectedCrew[0])}, ${buildCrewArray(selectedCrew[1].name)}, and ${buildCrewArray(selectedCrew[2].name)} are going to space!`)
