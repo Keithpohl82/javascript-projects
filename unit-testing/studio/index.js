57a03d9002411861364f177ee2a9b3598337d2a1
keithpohl82 <keith.pohl82@gmail.com>
2024-07-15 14:57:08 -0500

    studio setup.

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,6 +1,12 @@
 
 let launchcode = {
-  
+  organization: "nonprofit",
+  executiveDirector: "Jeff",
+  percentageCollEmployees: 100,
+  programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+  lounchOutput: function(){
+    
+  }
 }
 
 module.exports = launchcode;
