f9de3df7f1e4c83ebfd0b54923bc89ad557cc3f0
keithpohl82 <keith.pohl82@gmail.com>
2024-08-12 20:32:36 -0500

    studio 16 fetch jason

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,37 @@
 //TODO: Add Your Code Below
+window.addEventListener("load", function (){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (response) {
+        response.json().then(function (json){
+            console.log(response);
+            const container = document.getElementById("container");
+                     
+            let color = "";
+            for(const id in json){
+              const status = json[id].active;
+              if(status){
+                color = "green";
+              }
+              else{
+                color = "red"
+              }
+              console.log(status);
+                container.innerHTML += `
+                <div class="astronaut">
+                <div class="bio">
+                <h3>${json[id].firstName} ${json[id].lastName}</h3>
+                  <ul>
+                    <li>Hours in space: ${json[id].hoursInSpace}</li>
+                    <li id="innertext" style="color:${color}">Active: ${json[id].active}</li>
+                    <li>Skills: ${json[id].skills}</li>
+                  </ul>
+                </div>
+                <img class="avatar" src="${json[id].picture}">
+                </div>
+                `;
+            };
+            document.body.insertAdjacentHTML("afterend", `<p>
+              Astronaut Count: ${json.length}
+              </p>`)
+        });
+    });  
+});
